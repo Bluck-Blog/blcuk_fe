ce2c0526df5cbd0f1104db8fba54cfb99fe59e62
dlsdk0601 <dlsdk0601@gmail.com>
2022-07-31 18:04:21 +0900

    myPage tabMenu onClick

--- components/myPage/component/MyPageTabMenu.js
+++ components/myPage/component/MyPageTabMenu.js
@@ -10,14 +10,30 @@ const MyPageTabMenu = () => {
   const onTabMenuClick = (text) => {
     setActiveTab(text);
   };
+
   return (
     <S.MyPageTabWrapper>
       <ul>
-        <S.MyPageTabList>내가 쓴 글</S.MyPageTabList>
-        <S.MyPageTabList>블랙리스트</S.MyPageTabList>
-        <S.MyPageTabList>모든 알림</S.MyPageTabList>
+        <S.MyPageTabList
+          active={activeTab === "myText"}
+          onClick={() => onTabMenuClick("myText")}
+        >
+          내가 쓴 글
+        </S.MyPageTabList>
+        <S.MyPageTabList
+          active={activeTab === "blackList"}
+          onClick={() => onTabMenuClick("blackList")}
+        >
+          블랙리스트
+        </S.MyPageTabList>
+        <S.MyPageTabList
+          active={activeTab === "notification"}
+          onClick={() => onTabMenuClick("notification")}
+        >
+          모든 알림
+        </S.MyPageTabList>
       </ul>
-      <div></div>
+      <div>카테고리</div>
     </S.MyPageTabWrapper>
   );
 };
--- styles/myPage/MyPageTabMenuStyle.js
+++ styles/myPage/MyPageTabMenuStyle.js
@@ -8,12 +8,15 @@ export const MyPageTabWrapper = styled.div`
 
 export const MyPageTabList = styled.li`
   width: 100%;
+  cursor: pointer;
   height: 40px;
   line-height: 2.5;
   text-align: center;
   margin-bottom: 10px;
   font-size: 16px;
-  color: ${(props) => props.theme.textColor};
+  color: ${(props) => (props.active ? "white" : props.theme.textColor)};
   border: 2px solid ${(props) => props.theme.textColor};
   border-radius: 10px;
+  background: ${(props) => (props.active ? props.theme.textColor : "none")};
+  opacity: 0.9;
 `;
